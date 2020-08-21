58b6b70adb18b8abc3b5068128d7ce58ea4bd9a5
RCCodeBase <rahultdpz143@gmail.com>
2020-08-21 23:30:52 +0530

    UI Changes

---
+++ build/precache-manifest.e4a5643c793afb6b9f262c92690585f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c483a558f5e760faa58d44d5659e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063cc2c438be0b5fc4a4",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cc2a86af84b8c0ed3be2",
+    "url": "/static/css/main.b307898b.chunk.css"
+  },
+  {
+    "revision": "063cc2c438be0b5fc4a4",
+    "url": "/static/js/2.fd26478e.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/static/js/2.fd26478e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc2a86af84b8c0ed3be2",
+    "url": "/static/js/main.4a36935c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);

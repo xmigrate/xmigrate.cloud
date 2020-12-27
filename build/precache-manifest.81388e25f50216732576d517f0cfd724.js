89661244811adb8a77c8c8ebbe809f49c97e391f
RCcodeBase <rahultdpz143@gmail.com>
2020-12-27 23:21:16 +0530

    commiting

---
+++ build/precache-manifest.81388e25f50216732576d517f0cfd724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5cc224b98558987e35626025c9dd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4538e62a74d61674e695",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d8e6c6f132864ea25c21",
+    "url": "/static/css/main.62cd5eec.chunk.css"
+  },
+  {
+    "revision": "4538e62a74d61674e695",
+    "url": "/static/js/2.7662b4cb.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/static/js/2.7662b4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e6c6f132864ea25c21",
+    "url": "/static/js/main.da40dc6a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);

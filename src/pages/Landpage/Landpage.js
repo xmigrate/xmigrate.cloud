7300cf1b0335ad54912c1a41f4886960e335d6fa
akhilprakash634 <akhilprakash634@gmai.com>
2023-10-04 20:02:51 +0530

    form fix

--- src/pages/Landpage/Landpage.js
+++ src/pages/Landpage/Landpage.js
@@ -354,7 +354,7 @@ export default class Landpage extends Component {
           <Row className="justify-content-md-center">
             <Col md="6">
               <div class="form-head">
-                <form class="form" netlify>
+                <form class="form" form name="signupbeta" method="POST" data-netlify="true">
                   <p class="form-title">Sign in up for Beta</p>
                   <h3 class="form-sub-title">start your 30-day free trial</h3>
 

7a41731599fc99d7e5267206c8d54bbd96447254
akhilprakash634 <akhilprakash634@gmai.com>
2023-12-15 12:41:51 +0530

    added navigation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             <div className=" text-center text-indigo-300 text-lg font-normal font-['Inter'] leading-[31px] px-[20px] lg:px-[160px] xl:px-[420px]">Xmigrate(pronounced as cross-migrate) is an opensource tool for migrating your VM'sfrom anywhere to cloud and cloud to anywhere seamlessly.</div>
             <div className="justify-center items-start gap-6 inline-flex mt-[32px]">
                 <div className=" pl-[22.23px] pr-[21.77px] pt-[16.22px] pb-[16.78px] bg-slate-800 hover:bg-slate-700 rounded-[58px] justify-center items-center flex">
-                    <a href='https://github.com/xmigrate/xmigrate' target="_blank" className="w-[103px] h-5 text-center text-violet-300 hover:text-violet-200 text-base font-semibold font-['Inter'] leading-snug">Start Migrate</a>
+                    <a href='#contact' target="_blank" className="w-[103px] h-5 text-center text-violet-300 hover:text-violet-200 text-base font-semibold font-['Inter'] leading-snug">Start Migrate</a>
                 </div>
                 <div className=" pl-[25px] pr-[24.66px] pt-4 pb-[17.39px] bg-indigo-900 hover:bg-indigo-900 bg-opacity-20 rounded-[58px] border border-slate-800 justify-center items-center flex">
                     <a href='https://www.youtube.com/watch?v=FISJaoUIiOM' target="_blank" className="w-[90px] h-5 text-center text-violet-300 hover:text-violet-200 text-base font-semibold font-['Inter'] leading-snug">View Demo</a>
@@ -63,7 +63,7 @@ const Hero = () => {
                 </div>
                 <div className="max-w-3xl sm:px-3 px-2 text-center text-indigo-300 text-lg leading-[31px] tracking-wide">We are rigourosly testing and rolling out updates to our Xmigrate tool every week. Xmigrate team invite everyone to give this tool a try and give feedback. </div>
             </div>
-            <div id='#about' className="text-center mt-12 pt-12 text-indigo-100 text-4xl sm:text-5xl font-bold font-['Inter'] leading-[50px] sm:leading-[60px] max-w-[350px] sm:max-w-lg">Empower your cloud<span className="text-indigo-300"> migration</span></div>
+            <div id="features" className="text-center mt-12 pt-12 text-indigo-100 text-4xl sm:text-5xl font-bold font-['Inter'] leading-[50px] sm:leading-[60px] max-w-[350px] sm:max-w-lg">Empower your cloud<span className="text-indigo-300"> migration</span></div>
             <div className="text-center text-indigo-300 text-[20px] leading-7 tracking-tight mt-[28px]  px-2 sm:px-6">Xmigrate team invite everyone to give this tool a try and give feedback. </div>
         </div>
         //  </div>
